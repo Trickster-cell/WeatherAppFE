4986614a118e48c0ce79ae7ec7dd2fa3e0fba477
Trickster-cell-Y <76389577+Trickster-cell@users.noreply.github.com>
2024-10-21 04:36:01 +0530

    minor changes

--- components/ButtonGroup.jsx
+++ components/ButtonGroup.jsx
@@ -26,6 +26,34 @@ const ButtonGroup = () => {
             >
               Today's History{" "}
             </label>
+            <svg
+              className="w-5 h-5 text-gray-800 dark:text-white mr-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+              data-tooltip-target="tooltip-animation1"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+            <div
+              id="tooltip-animation1"
+              role="tooltip"
+              className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-400"
+            >
+              <p className="w-40">
+                Daily data is updated automatically every 20 minutes.
+              </p>
+              <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
           </div>
         </li>
         <li className="w-full dark:border-gray-600">
@@ -40,10 +68,38 @@ const ButtonGroup = () => {
             />
             <label
               htmlFor="horizontal-list-radio-passport"
-              className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              className="w-full py-3 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
-              Last 7 days
+              Historical Data{" "}
             </label>
+            <svg
+              className="w-5 h-5 text-gray-800 dark:text-white mr-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+              data-tooltip-target="tooltip-animation"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+            <div
+              id="tooltip-animation"
+              role="tooltip"
+              className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-400"
+            >
+              <p className="w-40">
+                The historical data summary is updated every day at UTC 0:00.
+              </p>
+              <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
           </div>
         </li>
       </ul>
